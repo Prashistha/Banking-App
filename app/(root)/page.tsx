e5bcb5ff4d4e65c3a3cbdbd7762c6c1ed5f6b91c
Nihar Phansalkar <phansalkarnihar@gmail.com>
2024-10-02 08:50:24 +0000

    added appwrite integration and added footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,57 @@
-import MobileNav from "../../components/components/MobileNav";
-import Sidebar from "../../components/components/SideBar";
-// import { getLoggedInUser } from "@/lib/actions/user.actions";
-import Image from "next/image";
-import { redirect } from "next/navigation";
+import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  // const loggedIn = await getLoggedInUser();
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
-  // if(!loggedIn) redirect('/sign-in')
+  if(!accounts) return;
+  
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId })
 
   return (
-    <main className="flex h-screen w-full font-inter">
-      {/* <Sidebar user={loggedIn} /> */}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-      <div className="flex size-full flex-col">
-        <div className="root-layout">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
-          <div>
-            {/* <MobileNav user={loggedIn} /> */}
-          </div>
-        </div>
-        {children}
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
-    </main>
-  );
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
+  )
 }
+
+export default Home
